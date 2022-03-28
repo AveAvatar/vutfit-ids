072d4406f64212d831655603043c385ddc3ff252
AveAvatar <55451817+AveAvatar@users.noreply.github.com>
2022-03-28 17:41:55 +0200

    Updated entities
    updated multikino, kinosal, promitani, film
    deleted program

--- src/proj.sql
+++ src/proj.sql
@@ -7,39 +7,47 @@
 
 CREATE TABLE multikino
 (
-    id          INT  NOT NULL PRIMARY KEY,
-    jmeno       VARCHAR(255) NOT NULL,
-    adresa      VARCHAR(255) NOT NULL,
-    trzby       NUMBER       NOT NULL
+    id INT GENERATED AS IDENTITY NOT NULL PRIMARY KEY,
+    jmeno VARCHAR(255) NOT NULL,
+    adresa VARCHAR(255) NOT NULL, -- mozno jeste rozdelime na ulice,cislo,mesto atd.
+    trzby NUMBER DEFAULT 0 NOT NULL
 );
 
 CREATE TABLE kinosal
 (
-    cislo_salu NUMBER(2)    NOT NULL PRIMARY KEY,
-    pocet_rad  NUMBER       NOT NULL,
-    velikost   NUMBER       NOT NULL,
-    typ        VARCHAR(255) NOT NULL
+    cislo_salu INT GENERATED AS IDENTITY NOT NULL PRIMARY KEY,
+    pocet_rad INT NOT NULL,
+    velikost INT NOT NULL,
+    typ VARCHAR DEFAULT NULL,
+    multikino_id INT DEFAULT NULL,
+    CONSTRAINT "multikino_id_fk"
+    	FOREIGN KEY (multikino_id) REFERENCES multikino (id)
+	ON DELETE CASCADE
 );
 
 CREATE TABLE promitani
 (
-    id             INT          NOT NULL PRIMARY KEY,
-    cislo_sal      INT          NOT NULL,
-    delka_projekce INT          NOT NULL,
-    zacatek        INT          NOT NULL,
-    konec          VARCHAR(255) NOT NULL,
-    typ_projekce   VARCHAR(8)   NOT NULL,
+    id INT GENERATED AS IDENTITY NOT NULL PRIMARY KEY,
+    delka_projekce INT NOT NULL,
+    zacatek TIMESTAMP NOT NULL,
+    konec TIMESTAMP  NOT NULL,
+    typ_projekce VARCHAR DEFAULT '2D' NOT NULL,
+    cislo_salu INT DEFAULT 0 NOT NULL,
+    film_id INT DEFAULT 0 NOT NULL,
     CONSTRAINT "cislo_salu_id_fk"
-		FOREIGN KEY (cislo_sal) REFERENCES kinosal (cislo_salu)
-		ON DELETE CASCADE
+    	FOREIGN KEY (cislo_salu) REFERENCES kinosal (cislo_salu)
+	ON DELETE CASCADE,
+    CONSTRAINT "film_id_fk"
+        FOREIGN KEY (film_id) REFERENCES  film(id)
+        ON DELETE CASCADE
 );
 
 CREATE TABLE film
 (
-    id      INT         NOT NULL PRIMARY KEY,
+    id  INT GENERATED AS IDENTITY NOT NULL PRIMARY KEY,
     dabing  VARCHAR(64) NOT NULL,
-    titulky VARCHAR(64) NOT NULL,
-    zanr    VARCHAR(64) NOT NULL
+    titulky VARCHAR(64) DEFAULT NULL,
+    zanr  VARCHAR(64) NOT NULL
 );
 
 CREATE TABLE zamestnanec
@@ -66,13 +74,6 @@ CREATE TABLE online_vstupenka
     stav_platby VARCHAR(16) NOT NULL
 );
 
-CREATE TABLE program
-(
-    id               INT          NOT NULL PRIMARY KEY,
-    vernostni_akce   VARCHAR(255) NOT NULL,
-    nabidka_projekci VARCHAR(255) NOT NULL,
-    vekove_kategorie VARCHAR(255) NOT NULL
-);
 
 CREATE TABLE zakaznik
 (
