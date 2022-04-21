854c1b812953f6d016900e9c68ddc4f71a588f28
AveAvatar <55451817+AveAvatar@users.noreply.github.com>
2022-04-21 15:43:52 +0200

    Triggers, Procedures, Materialized View Added

--- src/proj.sql
+++ src/proj.sql
@@ -24,7 +24,7 @@ CREATE TABLE vedouci
 
 CREATE TABLE multikino
 (
-    id INT GENERATED AS IDENTITY NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY,
     jmeno VARCHAR(255) NOT NULL,
     mesto VARCHAR(255) NOT NULL,
     ulice VARCHAR(255) NOT NULL,
@@ -38,7 +38,7 @@ CREATE TABLE multikino
 
 CREATE TABLE kinosal
 (
-    cislo_salu INT GENERATED AS IDENTITY NOT NULL PRIMARY KEY,
+    cislo_salu INT  default NULL PRIMARY KEY,
     pocet_rad INT NOT NULL,
     pocet_sedadel INT NOT NULL,
     typ VARCHAR(16) DEFAULT '2D' NOT NULL,
@@ -81,6 +81,7 @@ CREATE TABLE zamestnanec
     mesto   VARCHAR(255) NOT NULL,
     ulice  VARCHAR(255) NOT NULL,
     cislo_domu INT NOT NULL,
+    mzda INT NOT NULL,
     email VARCHAR(255)
 	    CHECK(REGEXP_LIKE(
 		    email, '^[a-z]+[a-z0-9\.]*@[a-z0-9\.-]+\.[a-z]{2,}$', 'i')),
@@ -161,17 +162,17 @@ VALUES (1);
 INSERT INTO VEDOUCI (id)
 VALUES (2);
 
-INSERT INTO MULTIKINO (jmeno, mesto,ulice, cislo_domu, trzby, vedouci_id)
-VALUES ('OC OLYMPIA' , 'Modřice', 'U Dálnice ', 3, '123456', 1);
-INSERT INTO MULTIKINO (jmeno, mesto, ulice, cislo_domu, trzby, vedouci_id)
-VALUES ('OC Velky Špalíček' , 'Brno', 'Mečová 695', 43, '456687', 2);
+INSERT INTO MULTIKINO (id, jmeno, mesto,ulice, cislo_domu, trzby, vedouci_id)
+VALUES (1000, 'OC OLYMPIA' , 'Modřice', 'U Dálnice ', 3, 123456, 1);
+INSERT INTO MULTIKINO (id, jmeno, mesto, ulice, cislo_domu, trzby, vedouci_id)
+VALUES (2000, 'OC Velky Špalíček' , 'Brno', 'Mečová 695', 43, 456687, 2);
 
 INSERT INTO KINOSAL (pocet_rad, pocet_sedadel, multikino_id)
-VALUES (15, 250, 1);
+VALUES (15, 250, 1000);
 INSERT INTO KINOSAL (pocet_rad, pocet_sedadel, typ, multikino_id)
-VALUES (20, 300, '3D', 1);
+VALUES (20, 300, '3D', 1000);
 INSERT INTO KINOSAL (pocet_rad, pocet_sedadel, typ, multikino_id)
-VALUES (15, 250, '2D', 1);
+VALUES (15, 250, '2D', 1000);
 INSERT INTO KINOSAL (pocet_rad, pocet_sedadel, typ, multikino_id)
 VALUES (15, 250, '2D', 2);
 INSERT INTO KINOSAL (pocet_rad, pocet_sedadel, typ, multikino_id)
@@ -201,14 +202,14 @@ VALUES (180, '23-12-2021 14:00:00', '23-12-2021 17:00:00', 4, 4);
 INSERT INTO promitani( delka_projekce, zacatek, konec, cislo_salu, film_id)
 VALUES (100, '23-12-2021 11:00:00', '23-12-2021 12:40:00', 5, 3);
 
-INSERT INTO zamestnanec(jmeno, prijmeni, mesto, ulice, cislo_domu, email, telcislo, multikino_id, vedouci_id)
-VALUES('Pan', 'A', 'Modřice', 'Husova', 33, 'vedouci@multikino2.cz', 420111111111, 2, 2);
-INSERT INTO zamestnanec(jmeno, prijmeni, mesto, ulice, cislo_domu, email, telcislo, multikino_id, vedouci_id)
-VALUES('Pan', 'B', 'Brno', 'Česká', 23, 'vedouci@multikino1.cz', 420111111111, 1, 1);
-INSERT INTO zamestnanec(jmeno, prijmeni, mesto, ulice, cislo_domu, email, telcislo, multikino_id)
-VALUES('Paní', 'C', 'Brno', 'Hlavní', 30, 'zamestnanec3@multikino1.cz', 420111111111, 1);
-INSERT INTO zamestnanec(jmeno, prijmeni, mesto, ulice, cislo_domu, email, telcislo, multikino_id)
-VALUES('Pan', 'D', 'Brno', 'Jánská', 40, 'zamestnanec4@multikino1.cz', 420111111111, 1);
+INSERT INTO zamestnanec(jmeno, prijmeni, mesto, ulice, cislo_domu, mzda, email, telcislo, multikino_id, vedouci_id)
+VALUES('Pan', 'A', 'Modřice', 'Husova', 33, 45555, 'vedouci@multikino2.cz', 420111111111, 2, 2);
+INSERT INTO zamestnanec(jmeno, prijmeni, mesto, ulice, cislo_domu, mzda, email, telcislo, multikino_id, vedouci_id)
+VALUES('Pan', 'B', 'Brno', 'Česká', 23,  55655, 'vedouci@multikino1.cz', 420111111111, 1, 1);
+INSERT INTO zamestnanec(jmeno, prijmeni, mesto, ulice, cislo_domu, mzda, email, telcislo, multikino_id)
+VALUES('Paní', 'C', 'Brno', 'Hlavní', 30,  15555, 'zamestnanec3@multikino1.cz', 420111111111, 1);
+INSERT INTO zamestnanec(jmeno, prijmeni, mesto, ulice, cislo_domu, mzda, email, telcislo, multikino_id)
+VALUES('Pan', 'D', 'Brno', 'Jánská', 40,  25555, 'zamestnanec4@multikino1.cz', 420111111111, 1);
 
 INSERT INTO zakaznik(rc, jmeno, prijmeni, mesto, ulice, cislo_domu, email, telcislo)
 VALUES(7204250999 ,'Pan', 'Y', 'Brno', 'Hlavní', 12, 'panx@seznam.cz', 420111111111);
@@ -242,6 +243,153 @@ INSERT INTO VSTUPENKA (rada, sedadlo, tarif, typ, stav_platby, rezervace_id,  pr
 VALUES (5, 22, 'Dospely', 'Online', 'Zaplaceno', 2, 1);
 INSERT INTO VSTUPENKA (rada, sedadlo, tarif, typ, stav_platby, rezervace_id, promitani_id )
 VALUES (4, 23, 'Student', 'Online', 'Nezaplaceno', 3, 1);
+
+----------------------------------------- TRIGGERS -----------------------------------------
+
+
+CREATE SEQUENCE salCislo START WITH 10 increment by 10;
+CREATE OR REPLACE TRIGGER znaceni_salu
+    BEFORE INSERT ON KINOSAL
+    FOR EACH ROW
+BEGIN
+    IF :NEW.cislo_salu IS NULL THEN
+
+        :NEW.cislo_salu :=  :NEW.multikino_id + salCislo.currval ;
+    end if;
+
+END;
+
+CREATE OR REPLACE TRIGGER prevod_na_eura
+  BEFORE INSERT ON multikino
+  FOR EACH ROW
+BEGIN
+    :NEW.trzby := :NEW.trzby / 27;
+END;
+
+SELECT * FROM multikino;
+
+
+CREATE OR REPLACE TRIGGER kontrolaRodnehoCisla
+        BEFORE INSERT OR UPDATE OF rc ON zakaznik
+        FOR EACH ROW
+DECLARE
+        rc zakaznik.rc%TYPE;
+        rok VARCHAR(4);
+        mesic VARCHAR(2);
+        den VARCHAR(2);
+        koncovka VARCHAR(4);
+        boolRC BOOLEAN;
+        boolECP BOOLEAN;
+BEGIN
+        rc := :NEW.rc;
+        rok := SUBSTR(rc, 1, 2);
+        mesic := SUBSTR(rc, 3, 2);
+        den := SUBSTR(rc, 5, 2);
+
+        -- Kontrola na numericke znaky
+        IF (LENGTH(TRIM(TRANSLATE(rc, '0123456789', ' '))) != NULL) THEN
+                RAISE_APPLICATION_ERROR(-20203, 'Neplatne znaky v rodnem cisle!');
+        END IF;
+
+        -- Koncovka ma 4
+        IF (LENGTH(rc) = 10) THEN
+            koncovka := SUBSTR(rc, 7, 4);
+
+            -- musi byt delitelna 11
+            IF (MOD(rc,11) != 0) THEN
+                    RAISE_APPLICATION_ERROR(-20203, 'Rodne cislo neni delitelne 11!');
+            END IF;
+
+            IF (rok > 53) THEN
+                rok := rok + 1900;
+            ELSE
+                rok := rok + 2000;
+            END IF;
+
+        ELSIF (LENGTH(rc) = 9) THEN
+            koncovka := SUBSTR(rc, 7, 3);
+
+            IF (koncovka = 000) THEN
+                    RAISE_APPLICATION_ERROR(-20203, 'Nespravny pocet znaku pro rodne cislo!');
+            END IF;
+
+        ELSE
+                RAISE_APPLICATION_ERROR(-20203, 'Nespravny pocet znaku pro rodne cislo!');
+        END IF;
+
+        IF (mesic > 50) THEN
+            mesic := mesic - 50;
+        END IF;
+
+        IF (mesic > 20) THEN
+            boolRC := TRUE;
+            mesic := mesic - 20;
+        END IF;
+
+        IF (den > 40) THEN
+            boolECP := TRUE;
+            den := den - 40;
+        END IF;
+
+        IF (boolECP = TRUE AND boolRC = TRUE) THEN
+            RAISE_APPLICATION_ERROR(-20203, 'Neplatne cislo ECP/RC');
+        END IF;
+        ---------------------------------------
+        IF (mesic <= 0 AND mesic > 12) THEN
+            RAISE_APPLICATION_ERROR(-20203, 'Mesic mimo rozsah');
+        END IF;
+
+        IF (mesic <= 0 AND mesic > 31) THEN
+            RAISE_APPLICATION_ERROR(-20203, 'Den mimo rozsah');
+        END IF;
+
+        IF ((mesic = 4 OR mesic = 6 OR mesic = 9 OR mesic = 11) AND den > 30) THEN
+            RAISE_APPLICATION_ERROR(-20203, 'Den mimo rozsah');
+        ELSIF (mesic = 2) THEN
+            IF(den > 29) THEN
+                RAISE_APPLICATION_ERROR(-20203, 'Den mimo rozsah');
+            ELSIF (den = 29) THEN
+                IF (MOD(rok, 4) != 0) THEN
+                    IF (MOD(rok, 400) != 0) THEN
+                        RAISE_APPLICATION_ERROR(-20203, 'Rok nebyl prestupny');
+                    END IF;
+                ELSIF (((MOD(rok, 4) = 0) OR (MOD(rok, 400) = 0)) AND (MOD(rok,100) = 0)) THEN
+				    Raise_Application_Error (-20203, 'rok ' || rok ||' nebol priestupny!');
+			    END IF;
+
+
+            END IF;
+        END IF;
+
+END kontrolaRodnehoCisla;
+
+--------------------------------------- PROCEDURY ------------------------------------------
+
+-- dalsi procedura -- nejvice prodanych vsutpenek dle tarifu / kolik to je ze vsech plus kolik tvori online a hotove zaplacene
+
+
+CREATE OR REPLACE PROCEDURE prumernaMzda
+IS
+CURSOR pracovnik IS SELECT * FROM ZAMESTNANEC;
+act_row Zamestnanec%ROWTYPE;
+plat INT;
+pocet INT;
+BEGIN
+plat := 0;
+pocet := 0;
+OPEN pracovnik;
+LOOP
+FETCH pracovnik INTO act_row;
+EXIT WHEN pracovnik%NOTFOUND;
+plat := plat + act_row.mzda;
+pocet := pocet + 1;
+END LOOP;
+DBMS_OUTPUT.PUT_LINE('Prumerna mzda zamestnance v kine je CZK ' || ROUND(plat/pocet));
+EXCEPTION
+WHEN ZERO_DIVIDE THEN
+DBMS_OUTPUT.PUT_LINE('Sit multikin nema zadne zamestnance');
+end;
+
 
 ----------------------------------------- SELECTS ------------------------------------------
 
@@ -335,7 +483,40 @@ WHERE z.rc NOT IN (
     SELECT r.zakaznik_id
     FROM REZERVACE R
 );
+------------------------------------------- RIGHTS ------------------------------------------
 
+GRANT ALL ON FILM TO XMAKAI00;
+GRANT ALL ON KINOSAL TO XMAKAI00;
+GRANT ALL ON MULTIKINO TO XMAKAI00;
+GRANT ALL ON PROMITANI TO XMAKAI00;
+GRANT ALL ON REZERVACE TO XMAKAI00;
+GRANT ALL ON TRANSAKCE TO XMAKAI00;
+GRANT ALL ON VEDOUCI TO XMAKAI00;
+GRANT ALL ON VSTUPENKA TO XMAKAI00;
+GRANT ALL ON ZAKAZNIK TO XMAKAI00;
+GRANT ALL ON ZAMESTNANEC TO XMAKAI00;
+
+----------------------------------- MATERIALIZOVANY POHLED ----------------------------------
+-- still lotta work, TBD
+
+CREATE MATERIALIZED VIEW LOG ON ZAMESTNANEC WITH PRIMARY KEY, ROWID(jmeno, prijmeni, email, telcislo) INCLUDING NEW VALUES ;
+CREATE MATERIALIZED VIEW seznamZamestancu
+BUILD IMMEDIATE
+REFRESH FAST ON COMMIT
+ENABLE QUERY REWRITE
+AS
+SELECT ZAMESTNANEC.jmeno, zamestnanec.prijmeni, zamestnanec.id, zamestnanec.mzda
+FROM ZAMESTNANEC;
+
+DELETE FROM seznamZamestancu WHERE prijmeni = 'A';
+SELECT * FROM ZAMESTNANEC;
+
+commit ;
+SELECT * FROM seznamZamestancu;
+
+BEGIN
+prumernaMzda;
+END;
 ------------------------------------ ZOBRAZENÍ TABULEK --------------------------------------
 
 SELECT * FROM VEDOUCI;
